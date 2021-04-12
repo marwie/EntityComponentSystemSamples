e59fb3621eaac25bc71aa8544bab3905613816ea
Marcel Wiessler <marcel@gaisterhand.de>
2021-04-12 12:23:47 +0200

    dependencymanager getdependencies prefix throws exceptions

---
+++ ECSSamples/Assets/_BugReport/Patch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using Unity.Entities;
+using UnityEngine;
+
+public class Patch : MonoBehaviour
+{
+	private void Start()
+	{
+		var harmony = new Harmony("com.needle.patchexception");
+		Harmony.DEBUG = true;
+		var t = typeof(Entity).Assembly.GetType("Unity.Entities.ComponentDependencyManager");
+		Debug.Assert(t != null);
+		var m = t.GetMethod("GetDependency");
+		Debug.Assert(m != null);
+		var prefix = GetType().GetMethod(nameof(Prefix), BindingFlags.Static | BindingFlags.NonPublic);
+		Debug.Assert(prefix != null);
+		harmony.Patch(m, new HarmonyMethod(prefix));
+	}
+
+	private static void Prefix()
+	{
+		// doing nothing
+	}
+}
